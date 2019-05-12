6b8c2688ace10dadcc11c833689254ec61f3ae93
nikolap <nikolapestanac@gmail.com>
2019-05-12 16:41:29 +0200

    Add project files.

---
+++ UWSBOT/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace UWSBOT
+{
+    class Program
+    {
+        //token NDk0NTIwMDY0NDIzNDkzNjMy.XNgvhw.VkzfTRygSXX_aymFg4fkwaK3hBA
+        public static void Main(string[] args)
+        => new Program().MainAsync().GetAwaiter().GetResult();
+
+        public async Task MainAsync()
+        {
+        }
+
+        private Task Log(LogMessage msg)
+        {
+            Console.WriteLine(msg.ToString());
+            return Task.CompletedTask;
+        }
+    }
+}
